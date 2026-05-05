acb551a33cca316689cf34738c1fade478c4d3d8
vijay kumar chaurasiya <118093154+vijay-kc@users.noreply.github.com>
2026-05-05 20:21:10 +0530

    Change API base URL to production server

--- client/src/api/index.tsx
+++ client/src/api/index.tsx
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from "axios"
 
-const BaseUrl ="http://localhost:5050/api"
+const BaseUrl ="https://unifiedcode-sever.onrender.com"
 // GET  http://localhost:5050/api/languages
 // POST http://localhost:5050/api/submissions
 // GET  http://localhost:5050/api/submissions/{token}
